9fa29f1fd8bb2636d8b27dffe93f9d23d3969a63
SaltyBuckets <49991711+SaltyBuckets@users.noreply.github.com>
2020-10-23 15:33:35 +0530

    add vue for better scalability

--- script.js
+++ script.js
@@ -1,4 +1,42 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, {duration: 300});
+// @ts-nocheck
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems, { duration: 300 });
+
+  let app = new Vue({
+    el: '#app',
+    data: {
+      submissionsData: [
+        {
+          backgroundImage:
+            'https://lh4.googleusercontent.com/-XplyTa1Za-I/VMSgIyAYkHI/AAAAAAAADxM/oL-rD6VP4ts/w1184-h666/Android-Lollipop-wallpapers-Google-Now-Wallpaper-2.png',
+          projectName: 'HALWAPURI',
+          sourceCodeLink: 'https://github.com/VibhorJaiswal/Website',
+          description:
+            '  Project k baare M Sanchept m Jankaari.Project k baare M Sanchept m\
+          Jankaari.Project k baare M Sanchept m Jankaari.Project k baare M\
+          Sanchept m Jankaari.Project k baare M Sanchept m Jankaari.Project k\
+          baare M Sanchept m Jankaari.Project k baare M Sanchept m\
+          Jankaari.Project k baare M Sanchept m Jankaari.Project k baare M\
+          Sanchept m Jankaari.Project k baare M Sanchept m Jankaari.Project k\
+          baare M Sanchept m Jankaari.Project k baare M Sanchept m\
+          Jankaari.Project k baare M Sanchept m Jankaari.Project k baare M\
+          Sanchept m Jankaari.Project k baare M Sanchept m Jankaari.Project k\
+          baare M Sanchept m Jankaari.Project k baare M Sanchept m\
+          Jankaari.Project k baare M Sanchept m Jankaari.Project k baare M\
+          Sanchept m Jankaari.Project k baare M Sanchept m Jankaari. Project k\
+          baare M Sanchept m Jankaari.Project k baare M Sanchept m\
+          Jankaari.Project k baare M Sanchept m Jankaari.Project k baare M\
+          Sanchept m Jankaari.Project k baare M Sanchept m Jankaari.Project k\
+          baare M Sanchept m Jankaari.Project k baare M Sanchept m\
+          Jankaari.Project k baare M Sanchept m Jankaari.Project k baare M\
+          Sanchept m Jankaari.Project k baare M Sanchept m Jankaari.Project k\
+          baare M Sanchept m Jankaari.Project k baare M Sanchept m\
+          Jankaari.Project k baare M Sanchept m Jankaari.Project k baare M\
+          Sanchept m Jankaari.Project k baare M Sanchept m Jankaari.Project k\
+          baare M Sanchept m Jankaari. Project k baare M Sanchept m Jankaari.',
+        },
+      ],
+    },
   });
+});
