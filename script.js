e56fb12c585aca36b7d79b583e37e0647edc9ea6
VibhorJaiswal <69683278+VibhorJaiswal@users.noreply.github.com>
2020-10-24 19:26:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,29 @@
 document.addEventListener('DOMContentLoaded', function () {
   var elems = document.querySelectorAll('.sidenav');
   var instances = M.Sidenav.init(elems, { duration: 300 });
+  
+//   change
+  
+  console.log("connected");
+
+
+    if (window.innerWidth < 800) {
+        let temp = document.querySelectorAll('#card');
+        for (let i = 0; i < 4; i++) {
+            temp[i].setAttribute("class", "row");
+            temp[i].id = "";
+        }
+    }
+
+
+// materialize
+
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, { duration: 300 });
+});
+  
+//   change
 
   let app = new Vue({
     el: '#app',
